a1c09e2f13cfa1f2db202323ea6caa9e4803c973
刘小然 <mk9540@.com>
2020-11-26 23:01:18 +0800

    父工程和支付工程

---
+++ cloud2020/payment8001/src/main/java/cn/lxr/springcloud/PaymentMain8081.java
@@ -0,0 +1,15 @@
+package cn.lxr.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther lhx
+ * @create 2020/11/26
+ */
+@SpringBootApplication
+public class PaymentMain8081 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8081.class, args);
+    }
+}
