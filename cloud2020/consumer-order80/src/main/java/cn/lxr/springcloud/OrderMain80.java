e9ce8468ef01eacf97da6c4cf15ec05c3e7afc22
刘小然 <mk9540@.com>
2020-12-15 22:18:25 +0800

    add consumer

---
+++ cloud2020/consumer-order80/src/main/java/cn/lxr/springcloud/OrderMain80.java
@@ -0,0 +1,15 @@
+package cn.lxr.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lhx
+ * @create 2020/12/15
+ */
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+    }
+}
